5c8c900e16145b687d0327479cb20423191b9393
comjang2 <comjang2@empal.com>
2024-07-24 17:06:30 +0900

    if press "ESC" key then program is terminated also.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -15,8 +15,11 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+#        self.screen = pygame.display.set_mode(
+#            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Alien Invasion")
 
@@ -46,11 +49,11 @@ class AlienInvasion:
              self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
              self.ship.moving_left = True
-        elif event.key == pygame.K_q:
+        elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
              sys.exit()
 
 
-    def _check_keyup(self, event):
+    def _check_keyup_events(self, event):
         """키에서 손을 뗄 때 응답합니다."""
         if event.key == pygame.K_RIGHT:
              self.ship.moving_right = False
